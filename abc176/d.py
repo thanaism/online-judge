5cf527129689f3c4ef723411b296383037876d90
thanai-mac <examle@examle.com>
2020-08-24 10:27:39 +0900

    task-d got TLE

--- abc176/d.py
+++ abc176/d.py
@@ -1,40 +1,19 @@
-h,w=map(int,input().split())
-ch,cw=map(int,input().split())
-dh,dw=map(int,input().split())
-s=[list(str(input())) for _ in range(h)]
-
-def prints():
-  for yy in range(h):
-    for xx in range(w):
-      print(s[yy][xx],end='')
-    print()
-
-def dfs(x,y):
-  s[x][y]='#'
-  prints()
-  for i in [-1,1]:
-    nx = x+i
-    ny = y
-    if nx >=0 and nx < w and ny >=0 and ny < h and s[ny][nx]=='.':
-      if ny==ch-1 and nx==cw-1:
-        return True
-      else:
-        if dfs(nx,ny):
-          return True
-  for j in [-1,1]:
-      nx = x
-      ny = y+j
-      if nx >=0 and nx < w and ny >=0 and ny < h and s[ny][nx]=='.':
-        if ny==ch-1 and nx==cw-1:
-          return True
-        else:
-          if dfs(nx,ny):
-            return True
-  return False
-
-res=0
-
-if dfs(dh-1,dw-1):
-  print('0')
+from collections import deque
+(h,w),(ch,cw),(dh,dw)=eval('map(int,input().split()),'*3)
+ss=[list(map(str, input())) for _ in range(h)]
+q = deque([(ch-1,cw-1,0)])
+while q:
+  j,i,c=q.popleft()
+  if ss[j][i]=='.':
+    ss[j][i]=c
+  for s in range(-2,3):
+    for t in range(-2,3):
+      if 0<=j+t<h and 0<=i+s<w and ss[j+t][i+s]=='.':
+        if abs(t)+abs(s)==1:
+          q.appendleft((j+t,i+s,c))
+        elif abs(t)+abs(s)>=2:
+          q.append((j+t,i+s,c+1))
+if ss[dh-1][dw-1] == '.':
+    print(-1)
 else:
-  print('-1')
+    print(ss[dh-1][dw-1])

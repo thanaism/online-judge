360e922efc3459509b2e89a07321d2a37741dd2b
thanai-mac <examle@examle.com>
2020-08-25 15:44:22 +0900

    e-8 got AC

--- ac-prob/abc086_b.py
+++ ac-prob/abc086_b.py
@@ -1,2 +1,11 @@
-ab=open(0).read()
-print(ab)
+ab=int(open(0).read().replace(' ',''))
+i=1
+flg=False
+while i<317:
+  if i**2 == ab:
+    flg=True
+  i+=1
+if flg:
+  print('Yes')
+else:
+  print('No')

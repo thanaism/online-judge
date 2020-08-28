b8518256acacda3ec77b1ff4d06ddf2e2df3bfcd
thanai-mac <examle@examle.com>
2020-08-28 14:47:18 +0900

    add agc047/

---
+++ atcoder/agc047/agc047-a.py
@@ -0,0 +1,44 @@
+# from sys import stdin
+# N = int(stdin.readline().rstrip())
+# # print(N)
+# ary=[]
+# for i in range(N):
+#   ary.append(float(stdin.readline().rstrip()))
+#   # stdin.readline().rstrip().split()
+#   # print(ary[i])
+# cnt = 0
+def get_divisible_count(num,divider):
+  result = 0
+  while True:    
+    if num%divider ==0:
+      result += 1
+      # num /= divider
+      num = int(num/divider)
+    else:
+      break
+  return result
+
+def is_divisible(num,divider,times):
+  result = 0
+  while True:    
+    if result ==times:
+      return True
+    elif num%divider ==0:
+      result += 1
+      # num /= divider
+      num = int(num/divider)
+    else:
+      break
+  return False
+
+print(get_divisible_count(27,3))
+print(is_divisible(27,3,4))
+
+# for i in range(N):
+
+#   for j in range(N-i-1):
+#     # print(i,j+i+1)
+#     # print(ary[i],ary[i+j+1],(((ary[i]%1)*(ary[j+i+1])%1)))
+#     if ((ary[i]%1)*(ary[j+i+1]%1))%1 == 0:
+#       cnt += 1
+# print(cnt)
---
+++ atcoder/agc047/agc047-b.py
@@ -0,0 +1,35 @@
+from sys import stdin
+N = int(stdin.readline().rstrip())
+
+ary=[]
+for i in range(N):
+  ary.append(str( stdin.readline().rstrip() ))
+
+cnt = 0
+
+def compareStr(str1,str2):
+  # print(str1,str2)
+  if str1 == str2:
+    return True
+  elif len(str1)>1:
+    if compareStr(str1[0]+str1[2:],str2):
+      # print(str1[0]+str1[2:],str2)
+      return True
+    elif compareStr(str1[1]+str1[2:],str2):
+      # print(str1[1]+str1[2:],str2)
+      return True
+    else:
+      return False 
+  else:
+    return False
+
+for i in range(N):
+  for j in range(N-i-1):
+    # print(i,j+i+1)
+    # print(ary[i],ary[i+j+1])
+    if compareStr(ary[i],ary[i+j+1]):
+      cnt += 1
+    elif compareStr(ary[i+j+1],ary[i]):
+      cnt += 1
+      
+print(cnt)

42b9310c4c7984b0a835dbcca27f54e099dfef25
thanai-mac <examle@examle.com>
2020-09-01 23:48:26 +0900

    shorten task d

--- atcoder/abc177/abc177_d.py
+++ atcoder/abc177/abc177_d.py
@@ -1,19 +1,24 @@
-l=lambda:map(int, input().split())
-n,m=l()
-p=[-1]*n
-def r(x):
-  if p[x]<0:return x
-  p[x]=r(p[x]);return r(p[x])
-def u(x,y):
-  x,y=r(x),r(y)
-  if x==y: return
-  p[x]+=p[y]
-  p[y]=x
-def e(x,y):
-  return r(x)==r(y)
-def s(x):
-  return -p[r(x)]
-for _ in range(m):
-  a,b=l()
-  u(a-1,b-1)
-print(-min(p))
+# l=lambda:map(int, input().split())
+# n,m=l()
+# p=[-1]*n
+# def r(x):
+#   if p[x]<0:return x
+#   p[x]=r(p[x]);return r(p[x])
+# def u(x,y):
+#   x,y=r(x),r(y)
+#   if x==y: return
+#   p[x]+=p[y]
+#   p[y]=x
+# def e(x,y):
+#   return r(x)==r(y)
+# def s(x):
+#   return -p[r(x)]
+# for _ in range(m):
+#   a,b=l()
+#   u(a-1,b-1)
+# print(-min(p))
+
+from networkx import*
+# _,*n=map(str.split,open(0))
+n=''
+print(max(*map(len,[*connected_components(Graph(n))]),1,1))

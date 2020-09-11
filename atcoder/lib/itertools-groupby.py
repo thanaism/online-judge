2c214f7441ee32a425df59d3c8651f77d4834cd0
thanai-mac <examle@examle.com>
2020-09-12 08:13:50 +0900

    modify itertools groupby

--- atcoder/lib/itertools-groupby.py
+++ atcoder/lib/itertools-groupby.py
@@ -1,35 +1,11 @@
-# ABC175-A
+# abc175_aに使用例あり
 from itertools import groupby
 mylist = [0,0,0,1,1,2,2,2,1,1,1]
 for key, group in groupby(mylist):
-  print(key, list(group),type(group))
+  print(key, list(group))
 
-# s = input()
-# if s == 'RRR':
-#   res = 3
-# elif s =='SRR':
-#   res = 2
-# elif s =='RSR':
-#   res = 1
-# elif s =='RRS':
-#   res = 2
-# elif s =='RSS':
-#   res = 1
-# elif s =='SRS':
-#   res = 1
-# elif s =='SSR':
-#   res = 1
-# else:
-#   res = 0
-# print(res)
-
-# s = input()
-# if 'RRR' in s:
-#     res = 3
-# elif 'RR' in s:
-#     res = 2
-# elif 'R' in s:
-#     res = 1
-# else:
-#     res = 0
-# print(res)
+# 出力
+# 0 [0, 0, 0]
+# 1 [1, 1]
+# 2 [2, 2, 2]
+# 1 [1, 1, 1]

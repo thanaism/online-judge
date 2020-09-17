a1c41d20c09df4c4ba98770fbc7b65774795df4d
thanai-mac <examle@examle.com>
2020-09-17 18:17:10 +0900

    task C,E got AC and short

---
+++ atcoder/abc178/abc178_e.py
@@ -0,0 +1,14 @@
+"""
+m,p=[],[]
+for i in [*open(0)][1:]:
+  x,y=map(int,i.split())
+  m.append(x-y)
+  p.append(x+y)
+f=lambda x:max(x)-min(x)
+print(max(f(m),f(p)))
+"""
+
+m,*p=[],
+for i in [*open(0)][1:]:x,y=map(int,i.split());m+=x-y,;p+=x+y,
+a=max;print(a(a(m)-min(m),a(p)-min(p)))
+

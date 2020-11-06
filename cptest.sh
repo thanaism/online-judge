0c54e8da2232eaa0e9663681503d7df95dab50e5
thanai-mac <examle@examle.com>
2020-11-06 13:35:36 +0900

    modify build-command of vscode for cpp files

--- cptest.sh
+++ cptest.sh
@@ -1,15 +1,12 @@
 #!/bin/zsh
+
 file_path=$1
-# echo $(pwd)
-# echo $(dirname ${file_name})
 file_name=$(basename ${file_path})
 dir_name=$(dirname ${file_path})
-# echo ${dir_name}
-# echo ${file_name}
+extension=${file_name##*.}
+echo $extension
 problem_name=${file_name%.*}
-# echo ${problem_name}
 test_dir=${dir_name}/test/${problem_name//-/_}
-# echo ${test_dir}
 base_url=${problem_name%_*}
 
 # GNU time
@@ -23,10 +20,9 @@ if [ ! -e ${test_dir} ]; then
     oj dl -d ${dir_name}/test/${problem_name//-/_} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
 fi
 
-# if [ $# = 2 ];then
-#     script_name=$2
-#     oj test -c "pypy3 ${script_name}.py" -d test/${problem_name//-/_}
-# else
-# echo ${dir_name}/test/${problem_name//-/_}
+if [ $extension = "cpp" ]; then
+    g++ ${file_path} && oj test -d ${dir_name}/test/${problem_name//-/_}
+else
     oj test -c "python ${file_path}" -d ${dir_name}/test/${problem_name//-/_}
-# fi
+fi
+

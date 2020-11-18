55d12d1647484bff560b2ebcec8f7c77f181ce02
thanai-mac <examle@examle.com>
2020-11-19 00:15:33 +0900

    AC

--- agc049/agc049_a.py
+++ agc049/agc049_a.py
@@ -1,3 +1,4 @@
+"""
 n=int(input())
 s=[]
 for _ in range(n):
@@ -22,3 +23,34 @@ def dfs(deleted,cnt):
       dfs(d,cnt+1)
 dfs(set(),0)
 print(sum(memo)/len(memo))
+"""
+
+# 入力
+n = int(input())
+s = [input() for _ in range(n)]
+# 出力の初期化
+ans = 0
+# 全頂点に関してループ
+for i in range(n):
+    # 頂点をキューに追加する
+    que = [i]
+    # 削除済かのフラグ
+    deleted = [False] * n
+    # キューが空になるまでループ
+    while que:
+        # キューから頂点を取り出し
+        v = que.pop()
+        # 削除済なら次の頂点へ
+        if deleted[v]: continue
+        # 削除済フラグを立てる
+        deleted[v] = True
+        # 頂点vから頂点uに辺があるかチェック
+        for u in range(n):
+            # 削除済または辺がない場合は次へ
+            if deleted[u] or s[u][v] == '0': continue
+            # 未削除の辺があれば頂点uをキューに追加
+            que.append(u)
+    # 頂点vから到達可能な全頂点の逆数を加える
+    ans += 1 / sum(deleted)
+# 出力
+print(ans)
--- agc049/agc049_b.py
+++ agc049/agc049_b.py
@@ -1,16 +1,51 @@
+"""
 n=int(input())
 s=[int(l) for l in input()]
 t=[int(l) for l in input()]
+u=[]
+for i,v in enumerate(s):
+  if v==1:u.append(i)
+u=u[::-1]
 cnt=0
 for i in range(n):
-  if s[i]!=t[i]:
-    j=i+1
-    while j<n and s[j]==0:
-      j+=1
-    if j==n:
-      print(-1)
-      exit()
-    s[j]=0
-    s[i]=(0 if s[i] else 1)
-    cnt+=j-i
+  if s[i]==1: u.pop()
+  if s[i]==t[i]: continue
+  if len(u)==0: break
+  j=u.pop()
+  cnt+=j-i
+  s[j]=0
+  s[i]=(0 if s[i] else 1)
+if s!=t:cnt=-1
 print(cnt)
+"""
+def solve():
+  # 入力
+  n=int(input())
+  s=[int(l) for l in input()]
+  t=[int(l) for l in input()]
+  # 累積XOR
+  xs=[0]
+  xt=[0]
+  for i in s: xs.append(xs[-1]^i)
+  for i in t: xt.append(xt[-1]^i)
+  print(xs,xt)
+  # S[j]!=S[j+1]となりうる最小のj
+  ans=j=0
+  for i in range(n+1):
+    # iがjを追い越したら更新
+    j=max(j,i)
+    # 操作不要ならスキップ
+    if xs[j]==xt[i]:continue
+    # Sが変化しない範囲でインクリメント
+    while j<n and xs[j]==xs[j+1]:j+=1
+    # 条件を満たすjがなければ不可能
+    if j==n:return -1
+    # S[j]==T[i]とするためインクリメント
+    j+=1
+    # 距離が操作回数と等しい
+    ans+=j-i
+  return ans
+# 出力
+print(solve())
+
+

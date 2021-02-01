e518b14aa49f3f412c5eda98727774bda75f8505
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-02-02 01:31:44 +0900

    AC

--- ac-prob/joi2008yo_e.py
+++ ac-prob/joi2008yo_e.py
@@ -1,7 +1,19 @@
-r,c=map(int,input().split())
-rc=[]
-for _ in range(r):
-    rc.append([*map(int,input().split())])
-for i in ranga(1<<r):
-    b=bin(i)[2::].zfill(r)
-    
+def main():
+    r,c=map(int,input().split())
+    l=[]
+    for _ in range(r):
+        l.append([*map(int,input().split())])
+    ans=0
+    for i in range(1<<r):
+        x=0
+        for j in range(c):
+            y=0
+            for k in range(r):
+                if (i>>k)&1:
+                    y+=(1,0)[l[k][j]]
+                else:
+                    y+=l[k][j]
+            x+=max(y,r-y)
+        ans=max(ans,x)
+    return ans
+print(main())

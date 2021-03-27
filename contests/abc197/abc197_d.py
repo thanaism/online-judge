ca6ff5cd1283f2c5ae5929a80adce06fb076c576
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-03-27 23:31:51 +0900

    attend a contest

---
+++ contests/abc197/abc197_d.py
@@ -0,0 +1,10 @@
+from math import *
+n=int(input())
+x0,y0=map(int,input().split())
+x2,y2=map(int,input().split())
+xo,yo=x0+(x2-x0)/2,y0+(y2-y0)/2
+x,y=x0-xo,y0-yo
+t=2*pi/n
+a=x*cos(t)-y*sin(t)
+b=x*sin(t)+y*cos(t)
+print(a+xo,b+yo)

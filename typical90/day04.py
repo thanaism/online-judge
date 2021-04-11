ee2da64ba65a938d3580f0e1ae54483a844c2ee9
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-04-11 17:44:14 +0900

    ac

---
+++ typical90/day04.py
@@ -0,0 +1,13 @@
+h,w=map(int,input().split())
+a=[]
+for _ in range(h):
+    a.append([*map(int,input().split())])
+r=[sum(i) for i in a]
+c=[0]*w
+for j in range(w):
+    for i in range(h):
+        c[j]+=a[i][j]
+for i in range(h):
+    for j in range(w):
+        print(r[i]+c[j]-a[i][j],end=' ')
+    print()

e37076b33ea5d0ffb4eae05c56d9a87952bb880e
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-04-23 22:44:33 +0900

    attend a contest

---
+++ contests/arc117/arc117_a.py
@@ -0,0 +1,8 @@
+a,b=map(int,input().split())
+c=list(range(-a,0))
+d=list(range(1,b+1))
+d[-1]-=sum(c)+sum(d)
+assert(sum(c+d)==0)
+assert(sum([1 for i in c if i<0])==b)
+assert(sum([1 for i in d if i>0])==a)
+print(*(c+d))
---
+++ contests/arc117/arc117_b.py
@@ -0,0 +1,27 @@
+n=int(input())
+a=list(map(int,input().split()))
+l=min(a)
+u=max(a)
+s=set(a)
+# print(s)
+t=list(s)
+t.sort()
+# print(t)
+b=0
+for i in range(1,len(t)):
+    if t[i]!=t[i-1]:
+        b+=(t[i]-t[i-1]-1)
+print(b)
+cnt=1
+for i in range(1,n):
+    if a[i]==a[i-1]:
+        cnt*=1
+print(cnt)
+ans=1<<u
+ans/=cnt
+ans%=10**9+7
+print(ans)
+print((1<<(u-b))%(10**9+7))
+diff=len(a)-len(t)
+ans=1<<(u-diff)
+print(ans%(1000000007))

9329adfe732d3533c3186efe9c2f27bf52667a05
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-04-23 22:49:27 +0900

    ac

--- rust/src/bin/typical90_a.rs
+++ rust/src/bin/typical90_a.rs
@@ -1,32 +1,32 @@
-fn main(){
-    proconio::input!{
-        n:isize,
-        l:isize,
-        k:isize,
-        mut a:[isize;n]
+fn main() {
+    proconio::input! {
+        n: isize,
+        l: isize,
+        k: isize,
+        mut a: [isize; n]
     }
     a.sort();
     let mut ok = 0;
-    let mut ng = l+1;
-    while (ng-ok).abs()>1 {
-        let mid = (ok+ng)/2;
-        if is_ok(&a,l,k,mid) {
+    let mut ng = l + 1;
+    while (ng - ok).abs() > 1 {
+        let mid = (ok + ng) / 2;
+        if is_ok(&a, l, k, mid) {
             ok = mid;
-        }else{
-            ng=mid;
+        } else {
+            ng = mid;
         }
     }
-    println!("{}",ok);
+    println!("{}", ok);
 }
 
-fn is_ok(a:&Vec<isize>,l:isize,k:isize,mid:isize)->bool{
-    let mut x=0;
-    let mut y=0;
+fn is_ok(a: &Vec<isize>, l: isize, k: isize, mid: isize) -> bool {
+    let mut x = 0;
+    let mut y = 0;
     for i in a {
-        if i-x>=mid && l-i>=mid{
-            x=*i;
-            y+=1;
+        if i - x >= mid && l - i >= mid {
+            x = *i;
+            y += 1;
         }
     }
-    y>=k
+    y >= k
 }

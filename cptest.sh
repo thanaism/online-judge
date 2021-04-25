3705e2076b825948c9d2ba7a78de8e0ea1ff0b45
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-04-26 00:19:20 +0900

    fix

--- cptest.sh
+++ cptest.sh
@@ -1,11 +1,10 @@
-
 file_path=$1
 file_name=$(basename ${file_path})
 dir_name=$(dirname ${file_path})
 extension=${file_name##*.}
 echo $extension
 problem_name=${file_name%.*}
-test_dir=${dir_name}/test/${problem_name//-/_}
+test_dir=${dir_name}/.test/${problem_name//-/_}
 base_url=${problem_name%_*}
 
 # GNU time
@@ -16,16 +15,16 @@ source .venv/bin/activate
 
 # make test directory
 if [ ! -e ${test_dir} ]; then
-    oj dl -d ${dir_name}/test/${problem_name//-/_} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
+    oj dl -d ${dir_name}/.test/${problem_name//-/_} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
 fi
 
 if [ $extension = "cpp" ]; then
-    g++ ${file_path} && oj test -d ${dir_name}/test/${problem_name//-/_}
+    g++ ${file_path} && oj test -d ${dir_name}/.test/${problem_name//-/_}
 elif [ $extension = "rs" ]; then
     # export RUST_BACKTRACE=1
-    cd rust && cargo build --release --bin ${problem_name} && cd .. && oj test -c "rust/target/release/${problem_name}" -d ${dir_name}/test/${problem_name//-/_} 
+    cd rust && cargo build --release --bin ${problem_name} && cd .. && oj test -c "rust/target/release/${problem_name}" -d ${dir_name}/.test/${problem_name//-/_} 
 else
-    oj test -c "python ${file_path}" -d ${dir_name}/test/${problem_name//-/_}
+    oj test -c "python ${file_path}" -d ${dir_name}/.test/${problem_name//-/_}
     # oj test -e 1e-6 -c "python ${file_path}" -d ${dir_name}/test/${problem_name//-/_}
 fi
 
--- cptest_eps.sh
+++ cptest_eps.sh
@@ -1,12 +1,10 @@
-#!/bin/zsh
-
 file_path=$1
 file_name=$(basename ${file_path})
 dir_name=$(dirname ${file_path})
 extension=${file_name##*.}
 echo $extension
 problem_name=${file_name%.*}
-test_dir=${dir_name}/test/${problem_name//-/_}
+test_dir=${dir_name}/.test/${problem_name//-/_}
 base_url=${problem_name%_*}
 
 # GNU time
@@ -17,13 +15,15 @@ source .venv/bin/activate
 
 # make test directory
 if [ ! -e ${test_dir} ]; then
-    oj dl -d ${dir_name}/test/${problem_name//-/_} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
+    oj dl -d ${dir_name}/.test/${problem_name//-/_} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
 fi
 
 if [ $extension = "cpp" ]; then
-    g++ ${file_path} && oj test -e 1e-6 -d ${dir_name}/test/${problem_name//-/_}
+    g++ ${file_path} && oj test -e 1e-6 -d ${dir_name}/.test/${problem_name//-/_}
+elif [ $extension = "rs" ]; then
+    # export RUST_BACKTRACE=1
+    cd rust && cargo build --release --bin ${problem_name} && cd .. && oj test -e 1e-6 -c "rust/target/release/${problem_name}" -d ${dir_name}/.test/${problem_name//-/_} 
 else
-    # oj test -c "python ${file_path}" -d ${dir_name}/test/${problem_name//-/_}
-    oj test -e 1e-6 -c "python ${file_path}" -d ${dir_name}/test/${problem_name//-/_}
+    oj test -e 1e-6 -c "python ${file_path}" -d ${dir_name}/.test/${problem_name//-/_}
+    # oj test -e 1e-6 -c "python ${file_path}" -d ${dir_name}/test/${problem_name//-/_}
 fi
-

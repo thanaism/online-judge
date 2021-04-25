4b5e379f40b3d6123abfdd4280b8da39f057bd62
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-04-26 00:44:55 +0900

    delete empty files

--- typical90/typical90_b.py
+++ typical90/typical90_b.py
@@ -4,11 +4,11 @@ for i in range(1<<n):
     if s.count('0')==s.count('1'):
         r=l=0
         ok=True
-        for j in range(n):
+        for j in sorted(range(n),reverse=True):
             if (i>>j)&1:
                 r+=1
             else:
                 l+=1
             if r>l:ok=False
         if ok:
-            print(s.replace('0', ')').replace('1', '('))
+            print(s.replace('0', '(').replace('1', ')'))

39010319f4bd567e9b27e3d5272708e09d2c9c61
thanai <62784012+thanaism@users.noreply.github.com>
2021-04-28 13:33:54 +0900

    ac

---
+++ rust/src/bin/typical90_x.rs
@@ -0,0 +1,13 @@
+fn main(){
+    proconio::input!{
+        n:usize,
+        k:isize,
+        a:[isize;n],
+        b:[isize;n]
+    }
+    let mut d=0;
+    for i in 0..n{
+        d+=(a[i]-b[i]).abs();
+    }
+    println!("{}",if d<=k && (k-d)%2==0{"Yes"}else{"No"});
+}

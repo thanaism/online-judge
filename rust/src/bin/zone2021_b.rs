ce6779f360b64948bb267cb4750ac12414af2347
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-05-02 22:50:58 +0900

    modify

--- rust/src/bin/zone2021_b.rs
+++ rust/src/bin/zone2021_b.rs
@@ -1,6 +1,6 @@
 fn main(){
     proconio::input!{n:i64,d:f64,h:f64,p:[(f64,f64);n]}
     let mut a=0f64;
-    for(i,j)in p{let b:f64=h-d*(h-j)/(d-i);if b>a{a=b}}
+    for(i,j)in p{a=a.max(h-d*(h-j)/(d-i))}
     println!("{}",a);
 }

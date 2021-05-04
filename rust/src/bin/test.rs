3979536cc904a2b3dbac46f1c7da7852ff2567ab
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-05-04 09:13:03 +0900

    implement lazy_static

--- rust/src/bin/test.rs
+++ rust/src/bin/test.rs
@@ -1,25 +1,29 @@
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+lazy_static!{
+    static ref H: Mutex<usize> = Mutex::new(0);
+    static ref W: Mutex<usize> = Mutex::new(0);
+    static ref V: Mutex<Vec<Vec<i32>>> = {
+        let h = *H.lock().unwrap();
+        let w = *W.lock().unwrap();
+        let v = vec![vec![0;w];h];
+        Mutex::new(v)
+    };
+}
 fn main() {
-    let mut m1: BTreeMap<isize, isize> = BTreeMap::new();
-    let mut m2: HashMap<isize, isize> = HashMap::new();
-    let mut s1:BTreeSet<isize> = BTreeSet::new();
-    let mut s2:HashSet<isize> = HashSet::new();
-    m1.insert(1, 1);
-    m1.insert(2, 1);
-    m1.insert(5, 2);
-    m1.insert(3, 2);
-    m2.insert(1, 1);
-    m2.insert(2, 1);
-    m2.insert(5, 2);
-    m2.insert(3, 2);
-    s1.insert(100);
-    s1.insert(80);
-    s1.insert(120);
-    s2.insert(100);
-    s2.insert(80);
-    s2.insert(120);
-    println!("{:?}",m1);
-    println!("{:?}",m2);
-    println!("{:?}",s1);
-    println!("{:?}",s2);
+    *H.lock().unwrap() = 5usize;
+    *W.lock().unwrap() = 5usize;
+    V.lock().unwrap()[0][1] = 500;
+    sub();
+    println!("{:?}",V.lock().unwrap());
+}
+fn sub() {
+    // let vec = &mut *V.lock().unwrap();
+    // let vec = &mut V.lock().unwrap();
+    let mut vec = V.lock().unwrap();
+    vec[1][1]=900;
+    vec[3][3]=350;
+    // V.lock().unwrap()[0][2] = 1000;
+    // V.lock().unwrap()[0][3] = 1000;
 }

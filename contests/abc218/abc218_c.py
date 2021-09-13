5d1fd54f90eaa069f02dcd6ac9142f9839547af5
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-09-14 00:28:17 +0900

    ac

---
+++ contests/abc218/abc218_c.py
@@ -0,0 +1,11 @@
+n=int(input())
+s=[complex(i,j) for i in range(n) for j,k in enumerate(input()) if k=='#']
+t=[complex(i,j) for i in range(n) for j,k in enumerate(input()) if k=='#']
+ans="No"
+for _ in range(4):
+    t=[z*1j for z in t]
+    t.sort(key=lambda z:(z.real,z.imag))
+    d=s[0]-t[0]
+    t=[z+d for z in t]
+    if s==t: ans="Yes"
+print(ans)

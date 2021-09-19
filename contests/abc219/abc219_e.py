b6d4d529cb37288adb846fc814c20210df197caf
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-09-19 15:05:33 +0900

    update

--- contests/abc219/abc219_e.py
+++ contests/abc219/abc219_e.py
@@ -1,22 +1,30 @@
+# Disjoint Set Union library
+class DSU:
+    def __init__(self,n):
+        self.parent=[-1]*n
+    def root(self,x):
+        if self.parent[x]<0:return x
+        self.parent[x]=self.root(self.parent[x])
+        return self.parent[x]
+    def merge(self,x,y):
+        rx=self.root(x)
+        ry=self.root(y)
+        if rx==ry:return
+        if rx>ry:rx,ry=ry,rx
+        self.parent[rx]+=self.parent[ry]
+        self.parent[ry]=rx
+    def size(self,x):
+        return -self.parent[self.root(x)]
+    def connected(self,x,y):
+        return self.root(x)==self.root(y)
+# Disjoint Set Union library
+
 b=int(''.join(c for c in input()+input()+input()+input() if c!=' ')[::-1],2)
 ex=[[0]*6 for _ in range(6)]
 ans=0
 for i in range(1<<16):
     if i&b!=b:continue
-    p=[-1]*36
-    def r(x):
-        if p[x]<0:return x
-        p[x]=r(p[x])
-        return p[x]
-    def u(x,y):
-        x=r(x)
-        y=r(y)
-        if x==y:return
-        if x>y:x,y=y,x
-        p[x]+=p[y]
-        p[y]=x
-    def s(x):
-        return -p[r(x)]
+    uft=DSU(36)
     t=0
     for j in range(4):
         for k in range(4):
@@ -25,7 +33,7 @@ for i in range(1<<16):
             if f:t=-~j*6-~k
     for j in range(6):
         for k in range(6):
-            if -~j<6 and ex[j][k]==ex[-~j][k]:u(j*6+k,-~j*6+k)
-            if -~k<6 and ex[j][k]==ex[j][-~k]:u(j*6+k,j*6-~k)
-    ans+=s(t)+s(0)==36
+            if -~j<6 and ex[j][k]==ex[-~j][k]:uft.merge(j*6+k,-~j*6+k)
+            if -~k<6 and ex[j][k]==ex[j][-~k]:uft.merge(j*6+k,j*6-~k)
+    ans+=uft.size(t)+uft.size(0)==36
 print(ans)

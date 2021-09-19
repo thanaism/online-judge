30c4c14c11584dedfb40ada8372e68591de7bbae
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-09-19 12:34:48 +0900

    shorten

--- contests/abc219/abc219_e.py
+++ contests/abc219/abc219_e.py
@@ -1,49 +1,35 @@
-from collections import deque
-a = [ [*map(int,input().split())] for _ in range(4) ]
-vs = { (i,j) for i in range(4) for j in range(4) if a[i][j] }
-ans = 0
+class DSU:
+	def __init__(self,n):
+		self.p=[-1]*n
+	def r(self,x):
+		if self.p[x]<0:return x
+		self.p[x]=self.r(self.p[x])
+		return self.p[x]
+	def u(self,x,y):
+		x=self.r(x)
+		y=self.r(y)
+		if x==y:return
+		if x>y:x,y=y,x
+		self.p[x]+=self.p[y]
+		self.p[y]=x
+	def s(self,x):
+		return -self.p[self.r(x)]
+	
+b=int(''.join(c for c in input()+input()+input()+input() if c!=' ')[::-1],2)
+ex=[[0]*6 for _ in range(6)]
+ans=0
 for i in range(1<<16):
-	ok = True
-	on  = [[False]*4 for _ in range(4)]
-	st_on = set()
-	off = [[True]*6 for _ in range(6)]
-	st_off = {(i,j) for i in range(6) for j in range(6)}
-	for j in range(16):
-		r = j//4
-		c = j%4
-		if (r,c) in vs and i>>j&1==0:
-			ok = False
-		if i>>j&1==1:
-			on[r][c] = True
-			st_on.add((r,c))
-			off[r+1][c+1] = False
-			st_off.remove((r+1,c+1))
-	if len(st_on)==0: continue
-	q = deque()
-	q.append(st_on.pop())
-	di = [0,1,0,-1]
-	dj = [1,0,-1,0]
-	while q:
-		a,b = q.popleft()
+	if i&b!=b:continue
+	u=DSU(36)
+	s=0
+	for j in range(4):
 		for k in range(4):
-			ni = a+di[k]
-			nj = b+dj[k]
-			if ni<0 or ni>=4 or nj<0 or nj>=4: continue
-			if not on[ni][nj] or (ni,nj) not in st_on: continue
-			q.append((ni,nj))
-			st_on.remove((ni,nj))
-	if len(st_on): ok = False
-	q = deque()
-	q.append(st_off.pop())
-	while q:
-		a,b = q.popleft()
-		for k in range(4):
-			ni = a+di[k]
-			nj = b+dj[k]
-			if ni<0 or ni>=6 or nj<0 or nj>=6: continue
-			if not off[ni][nj] or (ni,nj) not in st_off: continue
-			q.append((ni,nj))
-			st_off.remove((ni,nj))
-	if len(st_off): ok = False
-	if ok: ans+=1
+			f=i>>j*4+k&1
+			ex[-~j][-~k]=f
+			if f:s=-~j*6-~k
+	for j in range(6):
+		for k in range(6):
+			if -~j<6 and ex[j][k]==ex[-~j][k]:u.u(j*6+k,-~j*6+k)
+			if -~k<6 and ex[j][k]==ex[j][-~k]:u.u(j*6+k,j*6-~k)
+	ans+=u.s(s)+u.s(0)==36
 print(ans)

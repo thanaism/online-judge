d296f9d8f2408fb54058d044ca408ddd40ade615
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-09-19 12:44:22 +0900

    shorten

--- contests/abc219/abc219_e.py
+++ contests/abc219/abc219_e.py
@@ -1,35 +1,31 @@
-class DSU:
-	def __init__(self,n):
-		self.p=[-1]*n
-	def r(self,x):
-		if self.p[x]<0:return x
-		self.p[x]=self.r(self.p[x])
-		return self.p[x]
-	def u(self,x,y):
-		x=self.r(x)
-		y=self.r(y)
-		if x==y:return
-		if x>y:x,y=y,x
-		self.p[x]+=self.p[y]
-		self.p[y]=x
-	def s(self,x):
-		return -self.p[self.r(x)]
-	
 b=int(''.join(c for c in input()+input()+input()+input() if c!=' ')[::-1],2)
 ex=[[0]*6 for _ in range(6)]
 ans=0
 for i in range(1<<16):
-	if i&b!=b:continue
-	u=DSU(36)
-	s=0
-	for j in range(4):
-		for k in range(4):
-			f=i>>j*4+k&1
-			ex[-~j][-~k]=f
-			if f:s=-~j*6-~k
-	for j in range(6):
-		for k in range(6):
-			if -~j<6 and ex[j][k]==ex[-~j][k]:u.u(j*6+k,-~j*6+k)
-			if -~k<6 and ex[j][k]==ex[j][-~k]:u.u(j*6+k,j*6-~k)
-	ans+=u.s(s)+u.s(0)==36
+    if i&b!=b:continue
+    p=[-1]*36
+    def r(x):
+        if p[x]<0:return x
+        p[x]=r(p[x])
+        return p[x]
+    def u(x,y):
+        x=r(x)
+        y=r(y)
+        if x==y:return
+        if x>y:x,y=y,x
+        p[x]+=p[y]
+        p[y]=x
+    def s(x):
+        return -p[r(x)]
+    t=0
+    for j in range(4):
+        for k in range(4):
+            f=i>>j*4+k&1
+            ex[-~j][-~k]=f
+            if f:t=-~j*6-~k
+    for j in range(6):
+        for k in range(6):
+            if -~j<6 and ex[j][k]==ex[-~j][k]:u(j*6+k,-~j*6+k)
+            if -~k<6 and ex[j][k]==ex[j][-~k]:u(j*6+k,j*6-~k)
+    ans+=s(t)+s(0)==36
 print(ans)

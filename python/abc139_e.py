52b4122756fc7d5435a2c478d767870fb76b8e12
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-10-23 21:32:51 +0900

    ac

--- python/abc139_e.py
+++ python/abc139_e.py
@@ -1,52 +1,53 @@
-from collections import deque
+from collections import deque,defaultdict
+import sys
+
+def encode(i,j):
+    if i<j: i,j=j,i
+    return i*(i-1)//2 + j+1
 
 def solve(n,a):
-	endpoints = {} #defaultdict(list)
-	indegrees = {} #defaultdict(int)
-	for i in range(n):
-		for j in range(n-2):
-			s,t,u = i+1,a[i][j],a[i][j+1]
-			x = (s,t) if s<t else (t,s)
-			y = (s,u) if s<u else (u,s)
-			if x in endpoints:
-				endpoints[x].append(y)
-			else:
-				endpoints[x] = [y]
-			if y in indegrees:
-				indegrees[y] += 1
-			else:
-				indegrees[y] = 1
+    endpoints = defaultdict(list)
+    indegrees = defaultdict(int)
+    for i in range(n):
+        for j in range(1,n-1):
+            s,t,u = i+1,a[i][j-1],a[i][j]
+            x = encode(s,t)
+            y = encode(s,u)
+            endpoints[x].append(y)
+            indegrees[y] += 1
 
-	q = deque()
-	for i in range(n):
-		for j in range(i+1,n):
-			k = (i+1,j+1)
-			if k not in indegrees or indegrees[k]==0:
-				q.append(k)
-	s = set()
-	cnt = 0
-	ans = 1
-	while q:
-		i = q.popleft()
-		x,y = i
-		if x in s or y in s:
-			s = set()
-			ans += 1
-		s.add(x); s.add(y)
-		cnt += 1
-		if i not in endpoints: continue
-		for j in endpoints[i]:
-			indegrees[j] -= 1
-			if indegrees[j]==0:
-				q.append(j)
-				
-	if n*(n-1)//2>cnt: ans = -1
-	print(ans)
+    q = deque()
+    decode = defaultdict(None)
+    for i in range(n):
+        for j in range(i+1,n):
+            k = encode(i+1,j+1)
+            decode[k] = (i+1,j+1)
+            if indegrees[k]==0:
+                q.append(k)
+    s = set()
+    cnt = 0
+    ans = 1
+    while q:
+        i = q.popleft()
+        x,y = decode[i]
+        if x in s or y in s:
+            s = set()
+            ans += 1
+        s.add(x); s.add(y)
+        cnt += 1
+        for j in endpoints[i]:
+            indegrees[j] -= 1
+            if indegrees[j]==0:
+                q.append(j)
+                
+    if n*(n-1)//2>cnt: ans = -1
+    print(ans)
 
 def main():
-	n = int(input())
-	a = [[*map(int,input().split())] for _ in range(n)]
-	solve(n,a)
+    input = lambda: sys.stdin.readline().rstrip()
+    n = int(input())
+    a = [[*map(int,input().split())] for _ in range(n)]
+    solve(n,a)
 
 if __name__ == '__main__':
-	main()
+    main()

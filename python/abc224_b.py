1fa9e3fa65202af124eac3744334e811f8c86929
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-10-24 13:18:17 +0900

    ac

---
+++ python/abc224_b.py
@@ -0,0 +1,15 @@
+h,w = map(int,input().split())
+a = [[*map(int,input().split())] for _ in range(h)]
+
+ok = True
+for i1 in range(h):
+	for i2 in range(i1+1,h):
+		for j1 in range(w):
+			for j2 in range(j1+1,w):
+				if a[i1][j1]+a[i2][j2]>a[i2][j1]+a[i1][j2]:
+					ok = False
+
+if ok:
+	print('Yes')
+else:
+	print('No')

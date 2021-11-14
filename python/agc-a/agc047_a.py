8cf26e34b0606d2b192d15568fe6af252c81a549
thanai-mac <62784012+thanaism@users.noreply.github.com>
2021-11-14 23:06:18 +0900

    move python files

--- python/agc-a/agc047_a.py
+++ python/agc-a/agc047_a.py
@@ -1,32 +1,37 @@
 n = int(input())
-a = [int(float(input()) * (10 ** 9)) for _ in range(n)]
+a = []
+DIG = 10 ** 9
+for _ in range(n):
+    i = 0
+    s = input().split(".")
+    i += int(s[0]) * DIG
+    if len(s) > 1:
+        i += int((s[1] + "0" * 9)[:9])
+    a.append(i)
 
-
-def f(x, d):
-    res = 0
-    while True:
-        if x % d != 0:
-            break
-        x //= d
-        res += 1
-    return res
-
-
-l2 = [f(i, 2) for i in a]
-l5 = [f(i, 5) for i in a]
-cnt = [[0] * 100 for _ in range(100)]
+ds = {}
 for i in range(n):
-    cnt[l2[i]][l5[i]] += 1
+    two = five = 0
+    x = a[i]
+    while x % 2 == 0 and two < 18:
+        x //= 2
+        two += 1
+    x = a[i]
+    while x % 5 == 0 and five < 18:
+        x //= 5
+        five += 1
+    if (two, five) not in ds:
+        ds[(two, five)] = 0
+    ds[(two, five)] += 1
+
 ans = 0
-for i in range(19):
-    for j in range(19):
-        for k in range(19):
-            for l in range(19):
-                if i + k < 18 or j + l < 18:
-                    continue
-                if i == k and j == l:
-                    ans += cnt[i][j] * (cnt[k][l] - 1)
-                else:
-                    ans += cnt[i][j] * cnt[k][l]
-ans //= 2
-print(ans)
+for k1, v1 in ds.items():
+    for k2, v2 in ds.items():
+        if k1[0] + k2[0] < 18 or k1[1] + k2[1] < 18:
+            continue
+        if k1 == k2:
+            ans += v1 * (v1 - 1)
+        else:
+            ans += v1 * v2
+
+print(ans // 2)
